ae35941fc82ac2b4a44fa7470ca60ddc30e44d9b
Chris Gilmer <chris@truss.works>
2019-06-19 09:07:51 -0700

    Update module to terraform 0.12 syntax

--- main.tf
+++ main.tf
@@ -36,15 +36,15 @@
 #
 
 data "aws_eip" "nlb_eip1" {
-  id = "${var.nlb_eip_ids[0]}"
+  id = var.nlb_eip_ids[0]
 }
 
 data "aws_eip" "nlb_eip2" {
-  id = "${var.nlb_eip_ids[1]}"
+  id = var.nlb_eip_ids[1]
 }
 
 data "aws_eip" "nlb_eip3" {
-  id = "${var.nlb_eip_ids[2]}"
+  id = var.nlb_eip_ids[2]
 }
 
 #
@@ -55,46 +55,46 @@ resource "aws_lb" "main" {
   name               = "nlb-${var.name}-${var.environment}"
   load_balancer_type = "network"
 
-  enable_cross_zone_load_balancing = "${var.enable_cross_zone_load_balancing}"
+  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
 
   subnet_mapping {
-    subnet_id     = "${var.nlb_subnet_ids[0]}"
-    allocation_id = "${var.nlb_eip_ids[0]}"
+    subnet_id     = var.nlb_subnet_ids[0]
+    allocation_id = var.nlb_eip_ids[0]
   }
 
   subnet_mapping {
-    subnet_id     = "${var.nlb_subnet_ids[1]}"
-    allocation_id = "${var.nlb_eip_ids[1]}"
+    subnet_id     = var.nlb_subnet_ids[1]
+    allocation_id = var.nlb_eip_ids[1]
   }
 
   subnet_mapping {
-    subnet_id     = "${var.nlb_subnet_ids[2]}"
-    allocation_id = "${var.nlb_eip_ids[2]}"
+    subnet_id     = var.nlb_subnet_ids[2]
+    allocation_id = var.nlb_eip_ids[2]
   }
 
   tags = {
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
 
 resource "aws_lb_listener" "main" {
-  load_balancer_arn = "${aws_lb.main.id}"
-  port              = "${var.nlb_listener_port}"
+  load_balancer_arn = aws_lb.main.id
+  port              = var.nlb_listener_port
   protocol          = "TCP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.main.id}"
+    target_group_arn = aws_lb_target_group.main.id
     type             = "forward"
   }
 }
 
 resource "aws_lb_target_group" "main" {
   name = "ecs-${var.name}-${var.environment}-${var.container_port}"
-  port = "${var.container_port}"
+  port = var.container_port
 
   protocol    = "TCP"
-  vpc_id      = "${var.nlb_vpc_id}"
+  vpc_id      = var.nlb_vpc_id
   target_type = "ip"
 
   # The amount time for the NLB to wait before changing the state of a
@@ -103,19 +103,20 @@ resource "aws_lb_target_group" "main" {
 
   # Enable/Disable sending Proxy Protocol V2 headers
   # https://www.haproxy.org/download/1.8/doc/proxy-protocol.txt
-  proxy_protocol_v2 = "${var.enable_proxy_protocol_v2}"
+  proxy_protocol_v2 = var.enable_proxy_protocol_v2
 
   health_check {
-    protocol = "${var.health_check_protocol}"
-    port     = "${var.health_check_port}"
-    path     = "${var.health_check_protocol == "HTTP" || var.health_check_protocol == "HTTPS" ? var.health_check_path : ""}"
+    protocol = var.health_check_protocol
+    port     = var.health_check_port
+    path     = var.health_check_protocol == "HTTP" || var.health_check_protocol == "HTTPS" ? var.health_check_path : ""
   }
 
   # Ensure the NLB exists before things start referencing this target group.
-  depends_on = ["aws_lb.main"]
+  depends_on = [aws_lb.main]
 
   tags = {
-    Environment = "${var.environment}"
+    Environment = var.environment
     Automation  = "Terraform"
   }
 }
+
--- variables.tf
+++ variables.tf
@@ -1,66 +1,67 @@
 variable "container_port" {
   description = "The port on which the container will receive traffic."
   default     = 443
-  type        = "string"
+  type        = string
 }
 
 variable "health_check_protocol" {
   description = "The protocol that will be used for health checks.  Options are: TCP, HTTP, HTTPS"
   default     = "TCP"
-  type        = "string"
+  type        = string
 }
 
 variable "health_check_port" {
   description = "The port on which the container will receive health checks."
   default     = 443
-  type        = "string"
+  type        = string
 }
 
 variable "health_check_path" {
   description = "When using a HTTP(S) health check, the destination for the health check requests to the container."
-  type        = "string"
+  type        = string
   default     = "/"
 }
 
 variable "environment" {
   description = "Environment tag, e.g prod."
-  type        = "string"
+  type        = string
 }
 
 variable "enable_proxy_protocol_v2" {
   description = "Boolean to enable / disable support for proxy protocol v2."
   default     = "true"
-  type        = "string"
+  type        = string
 }
 
 variable "enable_cross_zone_load_balancing" {
   description = "If true, cross-zone load balancing of the load balancer will be enabled."
   default     = true
-  type        = "string"
+  type        = string
 }
 
 variable "name" {
   description = "The service name."
-  type        = "string"
+  type        = string
 }
 
 variable "nlb_eip_ids" {
   description = "List of Elastic IP allocation IDs to associate with the NLB. Requires exactly 3 IPs."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "nlb_listener_port" {
   description = "The port on which the NLB will receive traffic."
   default     = "443"
-  type        = "string"
+  type        = string
 }
 
 variable "nlb_subnet_ids" {
   description = "Subnets IDs for the NLB."
-  type        = "list"
+  type        = list(string)
 }
 
 variable "nlb_vpc_id" {
   description = "VPC ID to be used by the NLB."
-  type        = "string"
+  type        = string
 }
+
