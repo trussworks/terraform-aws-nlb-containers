6536edfbf51ad86ba8ec59f258c86bf7ff019dd5
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2019-11-06 13:55:39 -0800

    Remove NAT gateway, private subnets in terratest
    Prior to this change, the terratest created private subnets and a NAT
    gateway in the test VPC, which are not necessary.
    
    This change updates the simple terratest example to remove the NAT
    gateway and private subnets.

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -17,13 +17,11 @@ module "nlb_logs" {
 }
 
 module "vpc" {
-  source             = "terraform-aws-modules/vpc/aws"
-  version            = "~> 2"
-  cidr               = "10.0.0.0/16"
-  azs                = var.vpc_azs
-  private_subnets    = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
+  source         = "terraform-aws-modules/vpc/aws"
+  version        = "~> 2"
+  cidr           = "10.0.0.0/16"
+  azs            = var.vpc_azs
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 
 resource "aws_eip" "nlb" {
