1ba263609c275b72f0853a1f4c0e513946a8eb13
pjdufour-truss <patrick.dufour@truss.works>
2020-02-11 13:59:37 -0500

    update version number

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@
  *
  * ## Terraform Versions
  *
- * Terraform 0.12. Pin module version to ~> 2.X. Submit pull-requests to master branch.
+ * Terraform 0.12. Pin module version to ~> 2.0.2. Submit pull-requests to master branch.
  *
  * Terraform 0.11. Pin module version to ~> 1.5.0. Submit pull-requests to terraform011 branch.
  *
